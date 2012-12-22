0f1ebc314cdf8d9767c23b6a05c52bc5726ed708
John Else <john.else@gmail.com>
2012-12-22 06:23:01 +0000

    Rename banker_ui.py to banker_frontend.py
    Signed-off-by: John Else <john.else@gmail.com>

--- banker.py
+++ banker.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 
-import banker_ui
+import banker_frontend
 
 import snack
 
 if __name__ == "__main__":
     try:
         screen = snack.SnackScreen()
-        banker_ui.main(screen)
+        banker_frontend.main(screen)
     finally:
         screen.finish()
     
